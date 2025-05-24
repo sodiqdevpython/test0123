0dcff6a7fc722adf1635f0709e91254271fa95b1
Sodiq Oydinov <sodiqdevpython@gmail.com>
2025-05-24 06:43:31 +0500

    Debuging3

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -123,10 +123,10 @@ LOGOUT_URL = 'logout'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
-# ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
+]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
 
